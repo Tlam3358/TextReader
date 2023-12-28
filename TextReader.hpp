d1355e9edc7f6a42519dc298a56c6f15c969b7aa
Tlam3358 <50388430+Tlam3358@users.noreply.github.com>
2023-12-28 13:19:03 -0800

    Add files via upload

---
+++ TextReader.hpp
@@ -0,0 +1,97 @@
+#include <fstream>
+#include <vector>
+#include <string>
+#include <map>
+#include <iostream>
+#include "sanitize.hpp"
+
+using namespace std;
+
+class TextReader {
+
+private:
+
+    map<string, vector<string>> wordFollowers;
+
+public:
+    
+    void readIn(const string & filename);
+
+    bool isEndPunctuation(const char & character);
+
+    vector<string> getValues(const string & key);
+
+    bool search(const string& key);
+
+    size_t howManyfollowers(const string& key);
+    
+};
+
+//  TO DO
+
+//  COMPLETE THE FOLLOWING FUNCTIONS
+
+
+void TextReader::readIn(const string& filename) {
+/*std::ifstream textfile(filename);
+std::strin word;
+std::string old_word = "^";
+    while (textfile >> word){
+        textfile.ignore();
+        sanitize(word);
+    
+    if(isEndPunctuation(word[word.size() - 1])){
+        std::string punctuation(1, word[word.size() - 1]);
+        word = word.substr(0, word.size() - 1);
+        markov_chain[old_word].push_back(word);
+
+    }
+    }
+
+*/
+
+}
+
+
+bool TextReader::isEndPunctuation(const char& character) {
+
+if (character == ".")
+    return true;
+
+else if (character == "?")
+    return true;
+
+else if (character == "!")
+    return true;
+
+else
+    return false;
+
+
+
+}
+
+
+
+vector<string> TextReader::getValues(const string& key) {
+
+
+
+
+}
+
+
+
+bool TextReader::search(const string& key) {
+
+
+
+}
+
+
+size_t TextReader::howManyfollowers(const string& key) {
+
+
+
+
+}
